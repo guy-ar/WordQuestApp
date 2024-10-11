3fafa94b8c6dedef0b2358af150881bf88c72a8a
guy ar <guy.arbilly@gmail.com>
2024-10-11 23:33:51 +0300

    עדכון שדה רמת קושי להכיל גם שיעור + עדכון אפשרויות production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://word-quest-backend.onrender.com/api/',
+  PEXELS_API_KEY:'iGuqC3aWxrNxK2qcxxPoYlvXD7QrIGlEiDFB7TAhPUwZI48Z27KDafi6',
+  PEXELS_API_URL: 'https://api.pexels.com/v1/search'
 };
